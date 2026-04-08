9328f3bff0a3222ab723db288e4f750122393ce1
Alfi Zamriza <alfizamriza3@gmail.com>
2026-04-08 23:34:18 +0700

    feat: initialize root layout with metadata, global styles, providers, and Google Analytics tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,6 +30,22 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-QNGHQKZH24"
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-QNGHQKZH24');
+            `,
+          }}
+        />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
